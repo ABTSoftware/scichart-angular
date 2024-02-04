43148c544ef224a346cf16da69500d18e7b221fc
Aider Emirsuinov <aideremir@gmail.com>
2024-02-04 20:01:43 +0300

    add wasm

--- projects/scichart-angular/src/lib/scichart-angular.component.ts
+++ projects/scichart-angular/src/lib/scichart-angular.component.ts
@@ -1,16 +1,88 @@
 import { Component } from '@angular/core';
+import {
+  SciChartSurface,
+  NumericAxis,
+  FastLineRenderableSeries,
+  XyDataSeries,
+  EllipsePointMarker,
+  SweepAnimation,
+  SciChartJsNavyTheme,
+  NumberRange,
+  MouseWheelZoomModifier,
+  ZoomPanModifier,
+  ZoomExtentsModifier
+} from "scichart";
+
+async function initSciChart() {
+  // LICENSING
+  // Commercial licenses set your license code here
+  // Purchased license keys can be viewed at https://www.scichart.com/profile
+  // How-to steps at https://www.scichart.com/licensing-scichart-js/
+  // SciChartSurface.setRuntimeLicenseKey("YOUR_RUNTIME_KEY");
+
+  // Initialize SciChartSurface. Don't forget to await!
+  const { sciChartSurface, wasmContext } = await SciChartSurface.create("scichart-root", {
+    theme: new SciChartJsNavyTheme(),
+    title: "SciChart.js First Chart",
+    titleStyle: { fontSize: 22 }
+  });
+
+  // Create an XAxis and YAxis with growBy padding
+  const growBy = new NumberRange(0.1, 0.1);
+  sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { axisTitle: "X Axis", growBy }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { axisTitle: "Y Axis", growBy }));
+
+  // Create a line series with some initial data
+  sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
+    stroke: "steelblue",
+    strokeThickness: 3,
+    dataSeries: new XyDataSeries(wasmContext, {
+      xValues: [0,1,2,3,4,5,6,7,8,9],
+      yValues: [0, 0.0998, 0.1986, 0.2955, 0.3894, 0.4794, 0.5646, 0.6442, 0.7173, 0.7833]
+    }),
+    pointMarker: new EllipsePointMarker(wasmContext, { width: 11, height: 11, fill: "#fff" }),
+    animation: new SweepAnimation({ duration: 300, fadeEffect: true })
+  }));
+
+  // Add some interaction modifiers to show zooming and panning
+  sciChartSurface.chartModifiers.add(new MouseWheelZoomModifier(), new ZoomPanModifier(), new ZoomExtentsModifier());
+
+  return sciChartSurface;
+}
 
 @Component({
   selector: 'lib-scichart-angular',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      scichart-angular works!
-    </p>
+    <div>
+      <div id="scichart-root" style="width: 100%; height: 100%;"></div>
+    </div>
   `,
   styles: ``
 })
 export class ScichartAngularComponent {
+  chartInitializationPromise: Promise<SciChartSurface> = new Promise<SciChartSurface>(() => {});
 
+  ngOnInit(): void {
+    console.log("Angular: ngOnInit");
+    this.cleanupSciChart();
+    this.chartInitializationPromise = initSciChart(); // defined above
+  }
+
+  ngOnDestroy() {
+    console.log("Angular: ngOnDestroy");
+    this.cleanupSciChart();
+  }
+
+  cleanupSciChart() {
+    if (this.chartInitializationPromise) {
+      // Delete the chart from the DOM, and dispose of SciChart
+      this.chartInitializationPromise.then((sciChartSurface) => {
+        console.log("... Deleting SciChartSurface");
+        sciChartSurface.delete();
+      });
+      this.chartInitializationPromise = new Promise<SciChartSurface>(() => {});
+    }
+  }
 }
